cb0af68012a9e114901680ce901dcc31b7730d24
geppp41 <103062160+geppp41@users.noreply.github.com>
2024-12-01 00:20:48 -0800

    Hand.java fixed mixup in controlls Handler.java crated for a nice place to organize modules Main.java just a simple main class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/Main.java
@@ -2,11 +2,19 @@ package org.firstinspires.ftc.teamcode.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
+import org.firstinspires.ftc.teamcode.tests.Handler;
 @TeleOp
 public class Main extends LinearOpMode {
+    private static Handler handler = new Handler();
     @Override
     public void runOpMode() throws InterruptedException {
 
+        waitForStart();
+        handler.initilize();
+        if(opModeIsActive()) {
+            while (opModeIsActive()) {
+                handler.update();
+            }
+        }
     }
 }
